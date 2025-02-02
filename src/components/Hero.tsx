f491885ac767efd6b446de6f8a9f73f30492d39f
Victor Gomes <victorgomesdacosta18@gmail.com>
2025-02-01 21:20:15 -0300

    feat: implement hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,6 @@ import TaskTable from './Tascks';
 const Hero = () => {
   return (
     <main className="flex-1 flex-col p-6 bg-zinc-950">
-      {/* Hero Section */}
       <div className="w-full h-2/5 bg-cover bg-center" style={{ backgroundImage: "url('/assets/garden.jpeg')" }}>
         <div className="flex items-center justify-center w-full h-full">
           <h1 className="text-white text-4xl md:text-6xl lg:text-7xl font-bold text-center drop-shadow-lg">
@@ -13,7 +12,6 @@ const Hero = () => {
         </div>
       </div>
 
-      {/* TaskTable Section */}
       <div className="flex-1 flex justify-center items-center mt-6">
         <TaskTable />
       </div>
