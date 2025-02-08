9ebdcc8462017161e306cf95ed5a4fbe18462ac7
Victor Gomes <victorgomesdacosta18@gmail.com>
2025-02-08 18:40:18 -0300

    husky config

--- .husky/pre-commit.sh
+++ .husky/pre-commit.sh
@@ -2,7 +2,6 @@ echo 'Check for liting errors and run tests before commiting...'
 npm run check:fixgit ||
 (echo 'Liting errors or run testes failed. Please fix commiting' && exit 1)
 
-
 npm run check ||
 (echo 'Liting errors or run testes failed. Please fix commiting' && exit 1)
 
