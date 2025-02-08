f9e921913cdfe60aaf0c2e2921ae160127e5e5c8
Victor Gomes <victorgomesdacosta18@gmail.com>
2025-02-08 17:06:49 -0300

    fix: to correct erros

--- .husky/pre-commit.sh
+++ .husky/pre-commit.sh
@@ -1,1 +1,5 @@
-npm run check
+echo 'Check for liting errors and run tests before commiting...'
+npm run check ||
+(echo 'Liting errors or run testes failed. Please fix commiting' && exit 1)
+
+echo 'Done'
