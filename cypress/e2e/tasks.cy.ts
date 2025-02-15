cc2e760af49998cfb5de450c7a1038be487413c7
Victor Gomes <victorgomesdacosta18@gmail.com>
2025-02-14 22:06:05 -0300

    test: implement new switch or test

--- cypress/e2e/tasks.cy.ts
+++ cypress/e2e/tasks.cy.ts
@@ -14,4 +14,11 @@ describe('Tarefas - CRUD', () => {
       cy.contains('Nova Tarefa').should('be.visible');
     });
 
+    it('Deve adicionar uma nova tarefa', () => {
+      cy.visit('http://localhost:3000'); 
+      cy.get('[data-testid="input-task"]').type('Nova Tarefa');
+      cy.get('[data-testid="add-task"]').click();
+      cy.contains('Nova Tarefa').should('be.visible');
+    });
+
   });
